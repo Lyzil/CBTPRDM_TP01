6886ae8872302558ff296808cbeda3916f9f806c
Luiz Gustavo Verissimo Monteiro <gu.verissimomonteiro@gmail.com>
2026-09-09 22:11:37 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace TP_01
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOkClicked(object sender, EventArgs e)
+        {
+            // código do botão OK
+        }
+
+        private void OnLimparClicked(object sender, EventArgs e)
+        {
+            // código do botão Limpar
+        }
+
+        private void OnCreditosClicked(object sender, EventArgs e)
+        {
+            // código do botão Créditos
+        }
+    }
+}
